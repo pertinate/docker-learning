f5a8c30474dad8aa8f1cd2e7613d9102d13a4424
Nicholas Evans <ppertinate@gmail.com>
2020-10-16 00:13:37 -0500

    Added nodemon & a docker volume to watch index.js

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,19 @@
-FROM node:12.18.1
+FROM node:current
 
-WORKDIR /app
+ARG port=8080
+
+RUN mkdir -p /home/node/app/node_modules && chown -R node:node /home/node/app
+
+WORKDIR /home/node/app
 
 COPY package*.json ./
 
 RUN npm install
 
-COPY . .
+COPY --chown=node:node . .
 
-CMD [ "node", "src/index.js" ]
+ENV PORT=${port}
+
+EXPOSE ${port}
+
+CMD [ "npm", "start" ]
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
+const port = process.env.PORT || 8080;
+
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send(JSON.stringify({test: 'VALUE'}))
+    response.send(JSON.stringify({test: 'updated value'}))
 })
 
-app.listen(3000, () => {
-    console.log('Server Online')
+app.get('/docker', (request, response) => {
+    response.send(JSON.stringify({test: 'DOCKER'}))
 })
+
+app.listen(8080, () => {
+    console.log('Server Online on port:', port)
+});
