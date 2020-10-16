b6cb61cdddf895ff77c395fec9390a352ba27419
Nicholas Evans <ppertinate@gmail.com>
2020-10-16 01:48:08 -0500

    Testing if keeping config.json in the src directory gets built with the docker image. Add src/config.json to .gitignore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const config = require('./config.json');
+
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send(JSON.stringify({test: 'updated value'}))
+    response.send(JSON.stringify({test: 'updated vaSDFGSDGFDFGlue'}))
 })
 
 app.get('/docker', (request, response) => {
@@ -13,5 +15,6 @@ app.get('/docker', (request, response) => {
 })
 
 app.listen(8080, () => {
-    console.log('Server Online on port:', port)
+    console.log('Server Online on port THIS IS A CHANGE:', port)
+    console.log('config:', config)
 });
